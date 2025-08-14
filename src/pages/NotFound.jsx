bb2ed43aeaf5b60e8138092b286dea0b65d7405c
KelloVerra <71198584+KelloVerra@users.noreply.github.com>
2025-08-14 17:56:48 +0800

    ~Polished not found page (took way longer)

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,13 @@
-// import './NotFound.css'
 
 // Components
 import { useNavigate } from 'react-router'
 import Footer from '../comps/Footer'
+
+import styles from './NotFound.module.css'
+import notFoundArtBG from '/NotFoundArt.svg'
+import notFoundArtFG from '/NotFoundArtFG.svg'
+
+
 
 export default function NotFound() {
 
@@ -14,8 +19,13 @@ export default function NotFound() {
 
   return (
     <>
-      <h1>Not Found</h1>
-      <button onClick={goToHome}>Kembali</button>
+      <div className={styles['content']}>
+        <div className={styles['splash-container']}>
+          <img className={styles['splash-bg']} src={notFoundArtBG} />
+          <img className={styles['splash-fg']} src={notFoundArtFG} />
+        </div>
+        <button className={styles['back-button']} onClick={goToHome}>Kembali</button>
+      </div>
     </>
   )
 }
